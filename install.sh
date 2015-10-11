d3ac12b11661b13efa3728033d5e2a250bbed82d
Yann Kerhervé <yann.kerherve@gmail.com>
2015-10-11 09:21:14 -0700

    dotfiles: complete overhaul + fish
    - Don't need Makefile, shell scripts are enough
    - Add fish + z.fish
    - Remove a bunch of old stuff

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+DOTFILES=$(pwd)/data
+
+# git
+ln -sf ${DOTFILES}/gitconfig ${HOME}/.gitconfig
+ln -sf ${DOTFILES}/gitignore ${HOME}/.gitignore
+
+# vim
+mkdir -p ~/tmp
+mkdir -p ${HOME}/.vim/autoload
+curl -fLo ${HOME}/.vim/autoload/plug.vim \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -sf  ${DOTFILES}/vimrc ${HOME}/.vimrc
+
+# term multiplexers
+mkdir -p ${HOME}/.screen ${HOME}/.tmux
+ln -sf ${DOTFILES}/screenrc ${HOME}/.screenrc
+ln -sf ${DOTFILES}/tmux.conf ${HOME}/.tmux.conf
+# hopefully not needed anymore
+#ln -sf  ${DOTFILES}/termcaprc ${HOME}/.termcaprc
+
+# shells
+ln -sf ${DOTFILES}/bashrc ${HOME}/.bashrc
+ln -sf ${DOTFILES}/bash_profile ${HOME}/.bash_profile
+ln -sf ${DOTFILES}/profile ${HOME}/.profile
+
+mkdir -p ${HOME}/.config/fish
+ln -sf ${DOTFILES}/fish/config ${HOME}/.config/fish/config.fish
+ln -sf ${DOTFILES}/fish/functions ${HOME}/.config/fish/functions
---
+++ update-z.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir -p ${HOME}/._z
+
+curl -fLo ${HOME}/._z/bash https://raw.githubusercontent.com/rupa/z/master/z.sh
+curl -fLo ${HOME}/._z/fish https://raw.githubusercontent.com/sjl/z-fish/master/z.fish
