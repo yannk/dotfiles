227c493e4ddcd5aa5adab1150fc488cc5e4b0180
Yann Kerhervé <yann.kerherve@gmail.com>
2017-03-29 11:38:26 -0700

    bash: relocate-ssh to $HOME

--- install-rc.sh
+++ install-rc.sh
@@ -22,6 +22,7 @@ ln -sf ${DOTFILES}/tmux.conf ${HOME}/.tmux.conf
 ln -sf ${DOTFILES}/bashrc ${HOME}/.bashrc
 ln -sf ${DOTFILES}/bash_profile ${HOME}/.bash_profile
 ln -sf ${DOTFILES}/profile ${HOME}/.profile
+ln -sf ${DOTFILES}/relocate-ssh ${HOME}/.relocate-ssh.bash
 
 mkdir -p ${HOME}/.config/fish
 ln -sf ${DOTFILES}/fish/config ${HOME}/.config/fish/config.fish
