2db314f72cf0c552903dc8b90f3ebe0e9322eb41
Yann Kerhervé <yann.kerherve@gmail.com>
2021-02-14 16:05:17 -0800

    remove old stuff

--- install-rc.sh
+++ install-rc.sh
@@ -8,7 +8,7 @@ DOTFILES=$(pwd)/data
 ln -sf ${DOTFILES}/gitconfig ${HOME}/.gitconfig
 ln -sf ${DOTFILES}/gitignore ${HOME}/.gitignore
 ln -sf ${DOTFILES}/gitattributes ${HOME}/.gitattributes
-curl -o ${HOME}/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+#curl -o ${HOME}/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
 
 # vim
@@ -30,11 +30,8 @@ ln -sf ${DOTFILES}/relocate-ssh ${HOME}/.relocate-ssh.bash
 mkdir -p ${HOME}/.config/fish
 ln -sf ${DOTFILES}/fish/config ${HOME}/.config/fish/config.fish
 ln -snfF ${DOTFILES}/fish/functions ${HOME}/.config/fish/functions
-curl -fLo - https://raw.githubusercontent.com/fish-shell/fish-shell/master/share/functions/__fish_git_prompt.fish | sed 's/set -a/set/g' > ${HOME}/.config/fish/functions/__fish_git_prompt.fish
 
 mkdir -p ${HOME}/.config/nvim
 ln -sf ${DOTFILES}/init.nvim ${HOME}/.config/nvim/init.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-. update-z.sh
