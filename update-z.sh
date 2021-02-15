12e811ee5ef99477b1f13fd83685ed4d2b73b7bc
Yann Kerhervé <yann.kerherve@gmail.com>
2021-02-14 16:05:17 -0800

    remove old stuff

--- update-z.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -euo pipefail
-
-mkdir -p ${HOME}/._z
-
-curl -fLo ${HOME}/._z/bash https://raw.githubusercontent.com/rupa/z/master/z.sh
-curl -fLo ${HOME}/._z/fish https://raw.githubusercontent.com/sjl/z-fish/master/z.fish
